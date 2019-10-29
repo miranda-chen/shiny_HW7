a2feec64f7ff82d5c062a5a810d2173f88fe57b0
miranda-chen <mchen31@vosl.utk.edu>
2019-10-28 23:45:07 -0400

    Started code all over and changed my old diamonds shiny code to one based on mtcars

--- app.R
+++ app.R
@@ -1,72 +1,42 @@
-########
-# Shiny app to create a dynamically-filterable visualization of the diamonds app
-########
+####GEOL590 Class###
+### In-class Oct 23 - Intro to Shiny Part II ###
 
-# These bits get run before any of the rest of the code
-# Note: contrary to what I told you on Monday, the use of the global.R file is no longer recommended.
-# At present, I'm not sure why.
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
 library(shiny)
 library(tidyverse)
-mtcars
 
-# We'll limit the range of selectable carats to teh actual range of carats
+#set slider range to the range of carats in the dataset
 min.mpg <- min(mtcars$mpg)
 max.mpg <- max(mtcars$mpg)
-
-# Need a vector of axis variables as characters
-axis_vars <- names(mtcars)
-
-# Create a character vector of those columns of diamonds that are 
-factor.indices <- vapply(mtcars, is.factor, TRUE) # vapply is a base R function like sapply & lapply that we haven't talked about
-# It applies a function (is.factor) to every element of a list (diamonds, remember that a data frame is a list)
-# The 'TRUE' is at the end to convey that we want vapply to return the results as a logical vector:
-#   I could just as well have used FALSE or c(TRUE, FALSE, TRUE) or any other logical vector
-factor.columns <- axis_vars[factor.indices]
-
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
   
   # Application title
-  titlePanel("Diamonds viewer"),
+  titlePanel("mtcars dataset viewer"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-      
-      # This is a range slider (i.e. there's a max and min). It is set that way by "value" (the starting value), which is a 2-element vector
-      sliderInput("caratrange",
-                  "Range of carats",
-                  min = min.carat,
-                  max = max.carat,
-                  value = c(min.carat, max.carat)),
-      
-      
-      # Select x and y variables
-      selectInput(inputId = "xvar",
-                  label = "X axis",
-                  choices = axis_vars,
-                  selected = "x"),
-      
-      selectInput(inputId = "yvar",
-                  label = "Y axis",
-                  choices = axis_vars,
-                  selected = "y"),
-      
-      # I can't stop, I want to add a splash of color
-      selectInput(inputId = "color",
-                  label = "Color", 
-                  choices = factor.columns,
-                  selected = "carat"),
-      
-      actionButton("go", 
-                   "Go!",
-                   icon = icon("thumbs-up")) # see available icons at http://fontawesome.io/icons/ and http://getbootstrap.com/components/#glyphicons
+      sliderInput("mpg.adjuster",
+                  "Miles per gallon",
+                  min = min.mpg,
+                  max = max.mpg,
+                  value = c(min.mpg, max.mpg)),
+      submitButton(text = "Go!") #can make it a range, two things to adjust, not just single value
     ),
     
-    # Show a plot of diamonds data frame. This output doesn't care what that plot is, only that it will be associated with output$diamonds_plot
+    
+    # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("diamonds_plot")
+      plotOutput("mtcars_plot")
     )
   )
 )
@@ -74,36 +44,26 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-  # Filter diamonds based on carat - this doesn't strictly need to be in reactive,
-  # ...but maybe we want to expand the app later, in which case it'll be useful to have it in reactive()
-  filt_dia <- reactive({
-    diamonds %>%
-      filter(carat >= min(input$caratrange)) %>%
-      filter(carat <= max(input$caratrange))
+  #d_filt is a function
+  
+  d_filt <- reactive({
+    # Filter the mtcars plot so that it only contains the specific range of miles per gallon
+    low.mpg <- input$mpg.adjuster[1]
+    high.mpg <- input$mpg.adjuster[2]
+    
+    mtcars %>%
+      filter(mpg >= low.mpg) %>%
+      filter(mpg <= high.mpg)
+    
   })
   
-  # Make the plot
-  # eventReactive listens for a change in state of input$go, and only runs the code when the state changes
-  # Note use of aes_string, since input$xvar returns a string, not a reference to the object ("carat" not carat)
-  p_diamonds <- eventReactive(input$go, {
-    ggplot(filt_dia(), aes_string(x = input$xvar, y = input$yvar, colour = input$color)) + # Note that you need () after filt_dia, since filt_dia() is a function to get the object you want, not the actual object
+  output$mtcars_plot <- renderPlot({
+    ggplot(d_filt(), mapping = aes_string(x = "cyl", y = "hp", color = "vs")) + 
       geom_point()
+    
   })
-  
-  
-  # Create diagnostic output window to show what kind of output the double slider creates
-  output$diagnostic <- renderText(
-    input$caratrange
-  )
-  
-  # Create a dynamic plot plot
-  # I moved the ggplot into its own reactive context.
-  # Note that creating a ggplot object is very fast - it is actually drawing the plot that is slow.
-  output$diamonds_plot <- renderPlot(
-    p_diamonds()
-  )
-  
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
